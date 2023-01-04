eba8b8df173c4d0a5da3a1adcb11c42e6243c3c6
dshen <dxyshen@gmail.com>
2023-01-04 13:02:52 -0500

    cors headers

--- netlify/functions/hello-world-post.js
+++ netlify/functions/hello-world-post.js
@@ -1,3 +1,9 @@
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept",
+};
+
 export const handler = async (event, context, callback) => {
   // const { message, senderEmail, senderName } = JSON.parse(event.body);
   const data = JSON.parse(event.body);
@@ -6,17 +12,13 @@ export const handler = async (event, context, callback) => {
     return {
       statusCode: 200,
       body: data,
-      headers: {
-        "access-control-allow-origin": "*",
-      },
+      headers: CORS_HEADERS,
     };
   } catch (err) {
     return {
       statusCode: err.code,
       body: JSON.stringify({ msg: err.message }),
-      headers: {
-        "access-control-allow-origin": "*",
-      },
+      headers: CORS_HEADERS,
     };
   }
 };
